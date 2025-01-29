e6b9223bc15cbd4b9334da74b3ef1ee0b871bf9a
Md. Nafiur Rahman Protik <protiktee@users.noreply.github.com>
2025-01-29 22:43:39 +0600

    new update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,20 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
 using System;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 
-string input_1 = "1"; 
-string input_2 = "2";
+//string input_1 = "1"; 
+//string input_2 = "2";
 
-int sum = 0;
-//not correct
-sum = Convert.ToInt16(input_1 + input_2); 
-Console.WriteLine(sum);
-//correct
-sum = Convert.ToInt16(input_1  )+ Convert.ToInt16( input_2); 
-Console.WriteLine(sum);
-string msg = "";
+//int sum = 0;
+////not correct
+//sum = Convert.ToInt16(input_1 + input_2); 
+//Console.WriteLine(sum);
+////correct
+//sum = Convert.ToInt16(input_1  )+ Convert.ToInt16( input_2); 
+//Console.WriteLine(sum);
+//string msg = "";
 //try
 //{
 //    int i = Convert.ToInt16("");
@@ -24,14 +25,14 @@ string msg = "";
 //    msg = ex.Message;
 //}
 
-string NamePrefix = "Mr ";
-string Namesuffix = "Abc";
-//Console.WriteLine(msg);
+//string NamePrefix = "Mr ";
+//string Namesuffix = "Abc";
+////Console.WriteLine(msg);
 
-DateTime dateTime = DateTime.Now;   
-Console.WriteLine(dateTime.ToString("dd/MM/yyyy"));
-Console.WriteLine(dateTime.ToString("dd/MM/yyyy HH:mm"));
-Console.WriteLine(dateTime.ToString("dd/MM/yyyy HH:mm tt"));
+//DateTime dateTime = DateTime.Now;   
+//Console.WriteLine(dateTime.ToString("dd/MM/yyyy"));
+//Console.WriteLine(dateTime.ToString("dd/MM/yyyy HH:mm"));
+//Console.WriteLine(dateTime.ToString("dd/MM/yyyy HH:mm tt"));
 //www.c-sharpcorner.com/blogs/date-and-time-format-in-c-sharp-programming1
 /*
  dfdfd
@@ -40,12 +41,59 @@ Console.WriteLine(dateTime.ToString("dd/MM/yyyy HH:mm tt"));
 //dfdfdfdfdf
 
 
-string FromScreenDate = "27/01/2025";//01/27/2025
-DateTime dateTime1 = Convert.ToDateTime("02/01/2025");
-//DateTime.Now.ToString("dddd, dd MMMM yyyy")
-Console.WriteLine(dateTime1.ToString("dddd, dd MMMM yyyy"));
+//string FromScreenDate = "27/01/2025";//01/27/2025
+//DateTime dateTime1 = Convert.ToDateTime("02/01/2025");
+////DateTime.Now.ToString("dddd, dd MMMM yyyy")
+//Console.WriteLine(dateTime1.ToString("dddd, dd MMMM yyyy"));
 
-Console.WriteLine("------------------------");
-DateTime dateTime2 = DateTime.ParseExact("02/01/2025","dd/MM/yyyy",null);
-Console.WriteLine(dateTime2.ToString("dddd, dd MMMM yyyy"));
+//Console.WriteLine("------------------------");
+//DateTime dateTime2 = DateTime.ParseExact("02/01/2025","dd/MM/yyyy",null);
+//Console.WriteLine(dateTime2.ToString("dddd, dd MMMM yyyy"));
+
+
+
+//for (int i = 0; i < 6; i++)
+//{
+//    i++;
+//    Console.WriteLine(i);
+//    i = 6;
+//}
+
+//int i = 1;
+//while (i > 0)
+//{
+//    i++;
+//    Console.WriteLine(i);
+//    if (i == 10)
+//        i = 0;
+//}
+
+//string[] strArray = { "abc","xyz","pqr"};
+//int[] sArray = { 1,2,3 };
+//foreach (string item in strArray)
+//{
+//    Console.WriteLine(item);
+//}
+
+List<Human> strings = new List<Human>();
+Human human = new Human();
+human.Name = "Rahim";
+strings.Add(human);
+
+human = new Human();
+human.Name = "Karim";
+strings.Add(human);
+
+
+foreach (Human item in strings)
+{
+    Console.WriteLine("Person name is:"+item.Name);
+    Console.WriteLine("-----------------------");
+}
+
+ //  *
+ // ***
+ //*****
+
 Console.ReadLine();
+
